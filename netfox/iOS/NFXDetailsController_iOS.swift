86ab593d32621f08c4ed7243408c93e19a48507f
Elmee <37185272+elmeeee@users.noreply.github.com>
2025-09-20 16:43:27 +0700

    adding constrain

--- netfox/iOS/NFXDetailsController_iOS.swift
+++ netfox/iOS/NFXDetailsController_iOS.swift
@@ -78,6 +78,7 @@ class NFXDetailsController_iOS: NFXDetailsController, MFMailComposeViewControlle
 
         headerTopConstraint = headerContainer.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 12)
         NSLayoutConstraint.activate([
+            headerTopConstraint,
             headerContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             headerContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             headerContainer.heightAnchor.constraint(equalToConstant: 44)
