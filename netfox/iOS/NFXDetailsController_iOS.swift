31f5e06ac43714db60d862361427b4ce8f953f92
Elmee <37185272+elmeeee@users.noreply.github.com>
2025-09-20 16:45:51 +0700

    fixing headerTopConstraint

--- netfox/iOS/NFXDetailsController_iOS.swift
+++ netfox/iOS/NFXDetailsController_iOS.swift
@@ -77,12 +77,14 @@ class NFXDetailsController_iOS: NFXDetailsController, MFMailComposeViewControlle
         view.addSubview(headerContainer)
 
         headerTopConstraint = headerContainer.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 12)
-        NSLayoutConstraint.activate([
-            headerTopConstraint,
-            headerContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            headerContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            headerContainer.heightAnchor.constraint(equalToConstant: 44)
-        ])
+        if let headerTopConstraint {
+            NSLayoutConstraint.activate([
+                headerTopConstraint,
+                headerContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                headerContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                headerContainer.heightAnchor.constraint(equalToConstant: 44)
+            ])
+        }
 
         let sep = UIView()
         sep.backgroundColor = UIColor.black.withAlphaComponent(0.08)
